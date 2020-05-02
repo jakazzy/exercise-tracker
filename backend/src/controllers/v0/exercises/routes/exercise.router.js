744bfeb36c369d334e75ef32b47cc4321dd7eb3a
jakazzy <jakazzy@gmail.com>
2020-05-02 06:14:26 +0000

    add eercise router

---
+++ backend/src/controllers/v0/exercises/routes/exercise.router.js
@@ -0,0 +1,43 @@
+import { Router } from 'express'
+import { Exercise } from '../models/Exercise'
+
+const router = Router()
+
+router.post('/', async(req, res)=>{
+    const description = req.body.description;
+    const duration = req.body.duration;
+    
+
+    if( !description){
+        return res.status(422).send({message: "Description  cannot be null"})
+    }
+    if( !duration){
+        return res.status(422).send({ message: "Duration  cannot be null"})
+    }
+    
+   
+
+
+    const newExercise = await new Exercise({
+        description,
+        duration
+    })
+ 
+    let savedExercise;
+    try{
+        savedExercise = await newExercise.save()
+    }catch(e){
+        throw e
+    }
+ 
+    return res.status(201).send({ user: savedExercise})
+    
+    })
+
+    router.get('/', async(req, res)=>{
+        const items = await FeedItem.findAndCountAll({order:[['id', 'DESC']]})
+        return res.status(201).send(items)
+})
+
+ 
+    export const ExerciseRouter = router
