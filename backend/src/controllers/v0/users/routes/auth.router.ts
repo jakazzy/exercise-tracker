2a91199c85d19a07786c863af1e41cbb516c3631
jakazzy <jakazzy@gmail.com>
2020-05-15 05:49:26 +0000

    feat: create methods for comparing and generating passwords

--- backend/src/controllers/v0/users/routes/auth.router.ts
+++ backend/src/controllers/v0/users/routes/auth.router.ts
@@ -4,3 +4,15 @@ import * as jwt from 'jsonwebtoken'
 import {check, validationResult} from 'express-validator'
 import { User } from '../models/User'
 import { config } from '../../../../config/config'
+
+const router = Router()
+
+async function comparePasswords(plainTextPassword, hash){
+    return await bcrypt.compare(plainTextPassword, hash)
+}
+
+async function generatePasswords(plainTextPassword){
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds)
+    return await bcrypt.hash(plainTextPassword, salt)
+}
