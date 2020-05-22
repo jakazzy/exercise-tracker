cb467330bb25be059e21f98be251d52962d4f99f
jakazzy <jakazzy@gmail.com>
2020-05-22 11:22:12 +0000

    handling errors

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,2 +1,21 @@
+export function IssueTracker(){
+  this.issues = []
+}
 
-
+IssueTracker.prototype = {
+  add: function(issue){
+    this.issues.push(issue)
+  },
+  retrieve: function(){
+    let message = {}
+    switch (this.issues.length){
+      case 400:
+        message
+        break
+      case 401:
+        break
+      case 403:
+        break
+    }
+  },
+}
